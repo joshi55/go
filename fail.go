b2dcac5699d5eed3854b8622ba5db868bdadb75f
test <joshi55@users.noreply.github.com>
2021-11-30 14:45:44 +0000

    new files

---
+++ fail.go
@@ -0,0 +1,7 @@
+var myFloat float64 = 21.54
+var myInt int = 562
+var myInt64 int64 = 120
+
+var res1 = myFloat + myInt  // Not Allowed (Compiler Error)
+var res2 = myInt + myInt64  // Not Allowed (Compiler Error)
+
